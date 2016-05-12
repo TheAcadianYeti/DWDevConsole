658c241261c522dd9377ed413dd9fa042f56cd79
Patrick Williams <pwilliams@dsinet.deltaware.com>
2016-05-12 14:58:35 -0300

    Tried to add emailing, may not be possible locally

--- JS/ParkingWidget.js
+++ JS/ParkingWidget.js
@@ -25,23 +25,56 @@
 					comments: [],	
 				};
 				
-				
-				this.postUpdate  = function()
+				//Posts a comment
+				this.postComment = function()
 				{
-					$http.post("JSON/check.json?check=newCheck", _self.lastCheckIn, {"content-type" : "application/json"}).then(function(res)
+					this.config = {
+						method: "POST",
+						headers: {
+							'Content-Type': 'application/json',
+						},
+						url: 'JSON/check.json',
+						data: _self.lastCheckIn,
+						params: {postComment: 'true'}
+					}
+					$http(this.config).then(function(res)
 					{
-						//console.log(res.data);
+						console.log(res.data);
 					}, function(res)
 					{
-						//console.log(res.data);
+						console.log(res.data);
 					});
 				}
+				
+				//Post a check in update
+				this.postUpdate  = function()
+				{
+					this.config = {
+						method: 'POST',
+						headers: {
+							'Content-Type': 'application/json',	
+						},
+						url: 'JSON/check.json',
+						data: _self.lastCheckIn,
+						params: {postUpdate: 'true'}
+					};
+					//"JSON/check.json", _self.lastCheckIn, {"content-type" : "application/json"}
+					$http(this.config).then(function(res)
+					{
+						console.log(res.data);
+					}, function(res)
+					{
+						console.log(res.data);
+					});
+				}
+				
 				//Init function, loads data from the check.JSON file
 				this.init = function()
 				{
 					$http.get("JSON/check.json").then(function(res)
 					{
-						var obj = res.data.lastCheckIn;
+						console.log("Successfully loaded data");
+						var obj = res.data;
 						_self.lastCheckIn = obj;
 					}, function(res)
 					{
